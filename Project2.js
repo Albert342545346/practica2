8bdba5724b4b024f8e23c044f69f06ccfe3cbdc9
Albert342545346 <112861843+Albert342545346@users.noreply.github.com>
2025-11-23 16:08:57 +0300

    Update Project2.js

---
+++ Project2.js
@@ -0,0 +1,10 @@
+function Home() {
+  return (
+    <div className="page">
+      <h1>Добро пожаловать в Трекер технологий!</h1>
+      <p>Следи за своим прогрессом в изучении новых технологий.</p>
+    </div>
+  );
+}
+
+export default Home;
