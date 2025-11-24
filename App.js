e2b64c856dcfdc11c92c813bf97f156737cf4a07
Albert342545346 <112861843+Albert342545346@users.noreply.github.com>
2025-11-24 20:45:50 +0300

    1

---
+++ App.js
@@ -0,0 +1,30 @@
+// src/App.js
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navigation from './components/Navigation';
+import Home from './pages/Home';
+import TechnologyList from './pages/TechnologyList';
+import TechnologyDetail from './pages/TechnologyDetail';
+import AddTechnology from './pages/AddTechnology';
+import Stats from './pages/Stats';
+import Settings from './pages/Settings';
+
+function App() {
+  return (
+    <Router>
+      <div className="app">
+        <Navigation />
+        <main className="main-content">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/technologies" element={<TechnologyList />} />
+            <Route path="/technology/:techId" element={<TechnologyDetail />} />
+            <Route path="/add-technology" element={<AddTechnology />} />
+            {/* Новые маршруты */}
+            <Route path="/stats" element={<Stats />} />
+            <Route path="/settings" element={<Settings />} />
+          </Routes>
+        </main>
+      </div>
+    </Router>
+  );
+}
